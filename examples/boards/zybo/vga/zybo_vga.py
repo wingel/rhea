e6dfbdce58cf71eaf793b6f1eb4a66850682da98
Chris Felton <chris.felton@gmail.com>
2016-04-17 18:32:33 -0500

    changed default resolution to 800x600

--- examples/boards/zybo/vga/zybo_vga.py
+++ examples/boards/zybo/vga/zybo_vga.py
@@ -1,4 +1,6 @@
 
+from myhdl import Signal, intbv, always, always_comb
+
 from rhea.system import Global
 from rhea.cores.video import VGA
 from rhea.cores.video import VideoMemory
@@ -11,7 +13,7 @@ def zybo_vga(
     led, btn, vga_red, vga_grn, vga_blu,
     vga_hsync, vga_vsync, clock, reset=None,
     # Parameters
-    resolution=(640, 480), color_depth=(5, 6, 5),
+    resolution=(800, 600), color_depth=(5, 6, 5),
     refresh_rate=60, line_rate=31250):
     """
     This is a VGA example for the Digilent Zybo board.
@@ -48,5 +50,20 @@ def zybo_vga(
                           color_depth=color_depth)
     vga_inst = vga_sync(glbl, vga, vmem, resolution=resolution,
                         refresh_rate=refresh_rate, line_rate=line_rate)
+
+    bcnt = Signal(intbv(0, min=0, max=clock.frequency))
+    blink = Signal(bool(0))
+    
+    @always(clock.posedge)
+    def beh_blink():
+        if bcnt == clock.frequency-1:
+            bcnt.next = 0
+            blink.next = not blink
+        else:
+            bcnt.next = bcnt + 1
+
+    @always(clock.posedge)
+    def beh_led():
+        led.next = concat("000", blink)
 
     return bar_inst, vga_inst
